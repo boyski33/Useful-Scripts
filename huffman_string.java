6df62e1e63884f827b33e328406676b20cd0ec86
Boyan Kushlev <takarabukara@gmail.com>
2017-03-17 19:52:21 +0200

    real, working solution

--- huffman_string.java
+++ huffman_string.java
@@ -1,20 +1,16 @@
-void decode(String s, Node root){
-    for(int i = 0; i < s.length(); i++){
-        if(s.charAt(i) == '0'){
-            if(root.left != null){
-                decode(s.substring(i+1), root.left);
-            }
-            else{
-                System.out.print(root.data);
-            }
+void decode(String S ,Node root){
+    Node temp = root;
+    for(int i = 0; i < S.length(); i++){
+        if(S.charAt(i) == '0'){
+            temp = temp.left;
         }
         else{
-            if(root.right != null){
-                decode(s.substring(i+1), root.right);
-            }
-            else{
-                System.out.print(root.data);
-            }
+            temp = temp.right;
+        }
+        if(temp.left == null && temp.right == null){
+            System.out.print(temp.data);
+            temp = root;
         }
     }
+    
 }
