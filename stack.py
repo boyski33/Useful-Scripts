2609d121bcf6751298d3e0fdfbaf727204715886
Boyan Kushlev <takarabukara@gmail.com>
2017-03-19 13:49:04 +0200

    Update stack.py
    1 x - add x to the stack
    2 - remove top elements from the stack
    3 - return the max number in the stack (here we need an extra stack since without it the program times out)

--- stack.py
+++ stack.py
@@ -1,13 +1,36 @@
 class Stack(list):
-    def __init__(self, *items):
-        super().__init__([*items])
+    def __init__(self):
+        super().__init__()
+        self.max_nums = [0] # we need another stack for max nums since otherwise it times out for too many instances
 
     def push(self, item):
         self.append(item)
+        if item >= self.max_nums[-1]:
+            self.max_nums.append(item)
+            
 
     def is_empty(self):
         return not self
 
-s = Stack(1, 2, 3, 5)
-s.pop()
-print(s[-2])
+    def del_top(self):
+        if self[-1] == self.max_nums[-1]:
+            self.max_nums.__delitem__(-1)
+        self.__delitem__(-1)
+
+    def find_max(self):
+        return self.max_nums[-1]
+
+# main function where we test the class
+s = Stack()
+
+c = int(input())
+for i in range(c):
+    current = input()
+    comm = int(current[0])
+    if comm == 1:
+        num = current.split(" ")[1]
+        s.push(int(num))
+    elif comm == 2:
+        s.del_top()
+    elif comm == 3:
+        print(s.find_max())
