6603d9861557f9b8ef2bc1a7f597b440993ea953
Boyan Kushlev <takarabukara@gmail.com>
2017-04-07 13:00:13 +0300

    Update HashMapFill.java

--- HashMapFill.java
+++ HashMapFill.java
@@ -1,5 +1,5 @@
 Map<Integer, Integer> map = new HashMap<Integer, Integer>();
-Integer freq; //not int becase of possible NULL value
+Integer freq; //not int because of possible NULL value
 for(int i=0; i < n; i++){
     c[i] = in.nextInt();
     freq = map.get(c[i]);
