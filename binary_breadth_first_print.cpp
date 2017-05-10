b673c07147f9dc49d2015288448e72e90f1c8e18
Boyan Kushlev <takarabukara@gmail.com>
2017-05-10 13:31:36 +0300

    minor visual fixes

--- binary_breadth_first_print.cpp
+++ binary_breadth_first_print.cpp
@@ -5,12 +5,12 @@ void LevelOrder(node * root)
     queue<node*> q;
     node *temp = root;
 
-    while(temp!=NULL)
+    while(temp != NULL)
     { 
-        cout<<temp->data<<" ";
-        if(temp->left!=NULL)
+        cout << temp->data << " ";
+        if(temp->left != NULL)
             q.push(temp->left);
-        if(temp->right!=NULL)
+        if(temp->right != NULL)
             q.push(temp->right);
 
         temp = q.front();  
